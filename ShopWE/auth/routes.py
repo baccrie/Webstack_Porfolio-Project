76878226748a531aec5426ae5da30f8a37380ae5
baccrie <bakarerilwan1@gmail.com>
2023-06-24 17:48:44 +0100

    Customer registration models

--- ShopWE/auth/routes.py
+++ ShopWE/auth/routes.py
@@ -1,7 +1,8 @@
-from ShopWE import app, db
+from ShopWE import app, db, bcrypt, flash
 from flask import Blueprint, render_template, url_for, session, request, redirect
 from flask_login import login_required
 from ShopWE.customers.forms import CustomerRegister
+from ShopWE.models import Customer
 
 auth = Blueprint('auth', __name__)
 
@@ -12,8 +13,14 @@ def login():
 @auth.route('/auth/customer/register', methods=['POST', 'GET'])
 def customer_register():
     form = CustomerRegister()
-
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        customer = Customer(username=form.username.data, email=form.email.data, first_name=form.first_name.data,
+                            last_name=form.last_name.data, country=form.country.data, state=form.state.data, city=form.city.data,
+                            phone_number=form.number.data, password=hashed_password)
+        db.session.add(customer)
+        # db.session.commit()
+        flash(f'Welcome home {customer.username}', 'success')
         return redirect(url_for('home'))
     return render_template('auth/register.html', form=form)
 
